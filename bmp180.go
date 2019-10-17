6016c9c675d82ec23569b6e0cec178e3c3030207
Chuan Bai <chuan.bai@gmail.com>
2019-10-17 18:45:38 +0100

    Make indent consistent with original repo

--- bmp180.go
+++ bmp180.go
@@ -7,65 +7,65 @@
 package bmp180
 
 import (
-    "bytes"
-    "encoding/binary"
-    "time"
+	"bytes"
+	"encoding/binary"
+	"time"
 )
 
 // Device is the interface that groups methods used to communicate with an
 // underlying I2C device of any kind.
 type Device interface {
-    Close() error
-    Read(buf []byte) error
-    ReadReg(reg byte, buf []byte) error
-    Write(buf []byte) (err error)
-    WriteReg(reg byte, buf []byte) (err error)
+	Close() error
+	Read(buf []byte) error
+	ReadReg(reg byte, buf []byte) error
+	Write(buf []byte) (err error)
+	WriteReg(reg byte, buf []byte) (err error)
 }
 
 const (
-    regID               = 0xD0
-    regControl          = 0xF4
-    regTempOrPressure   = 0xF6
-    cmdReadTemp         = 0x2E
-    cmdReadPressure     = 0x34
-    regCalibration      = 0xAA
-    calibrationNumWords = 22
+	regID               = 0xD0
+	regControl          = 0xF4
+	regTempOrPressure   = 0xF6
+	cmdReadTemp         = 0x2E
+	cmdReadPressure     = 0x34
+	regCalibration      = 0xAA
+	calibrationNumWords = 22
 )
 
 type calibration struct {
-    ac1, ac2, ac3      int16
-    ac4, ac5, ac6      uint16
-    b1, b2, mb, mc, md int16
+	ac1, ac2, ac3      int16
+	ac4, ac5, ac6      uint16
+	b1, b2, mb, mc, md int16
 }
 
 // Sensor is a struct which holds methods and data to communmicate with a
 // BMP180 environmental sensor.
 type Sensor struct {
-    dev         Device      // the I2C communication device
-    calib       calibration // parsed calibration data
-    tempCelsius float64     // the latest measured temperature
+	dev         Device      // the I2C communication device
+	calib       calibration // parsed calibration data
+	tempCelsius float64     // the latest measured temperature
 }
 
 // NewSensor returns a pointer to a variable of type Sensor which has the I2C
 // communication device set. It reads calibration data from the I2C device
 // and pre-calculates constants needed for temperature and pressure calulations.
 func NewSensor(device Device) *Sensor {
-    s := new(Sensor)
-    s.dev = device
+	s := new(Sensor)
+	s.dev = device
 
-    readCalibration(s)
-    return s
+	readCalibration(s)
+	return s
 }
 
 // ID reads the chip ID from the underlying I2C device. The BMP180 sensor
 // always returns 0x55. This function can be used to test basic communication.
 func (s *Sensor) ID() (byte, error) {
-    buf := make([]byte, 1, 1)
-    err := s.dev.ReadReg(regID, buf)
-    if err != nil {
-        return 0, err
-    }
-    return buf[0], nil
+	buf := make([]byte, 1, 1)
+	err := s.dev.ReadReg(regID, buf)
+	if err != nil {
+		return 0, err
+	}
+	return buf[0], nil
 }
 
 // Temperature reads a raw temperature value from the underlying IC2 device.
@@ -73,146 +73,146 @@ func (s *Sensor) ID() (byte, error) {
 // data, stores the temperature as state information for the next pressure calculation
 // and returns the temperature.
 func (s *Sensor) Temperature() (float64, error) {
-    tu, err := readRawTemp(s)
-    if err != nil {
-        return 0, err
-    }
-    t := calcTempCelsius(tu, s.calib)
-    s.tempCelsius = t
-    return t, nil
+	tu, err := readRawTemp(s)
+	if err != nil {
+		return 0, err
+	}
+	t := calcTempCelsius(tu, s.calib)
+	s.tempCelsius = t
+	return t, nil
 }
 
 // Pressure reads a raw pressure value from the underlying IC2 device.
 // From this it then calculates a real pressure in millibars based on the
 // previously read temperature and calibration data and returns that pressure.
 func (s *Sensor) Pressure(oss uint8) (float64, error) {
-    tu, err := readRawTemp(s)
-    msb, lsb, xlsb, err := readRawPressure(s, oss)
-    if err != nil {
-        return 0, err
-    }
-    return calcPressurePascal(tu, s.tempCelsius, msb, lsb, xlsb, oss, s.calib), nil
+	tu, err := readRawTemp(s)
+	msb, lsb, xlsb, err := readRawPressure(s, oss)
+	if err != nil {
+		return 0, err
+	}
+	return calcPressurePascal(tu, s.tempCelsius, msb, lsb, xlsb, oss, s.calib), nil
 }
 
 // PressureSealevel does the same as Pressure, but calculates and returns the
 // sealevel pressure based on altitudeMeters at which the pressure has been measured.
 func (s *Sensor) PressureSealevel(oss uint8, altitudeMeters float64) (float64, error) {
-    p, err := s.Pressure(oss)
-    if err != nil {
-        return 0, err
-    }
-    return calcPressurePascalSealevel(p, altitudeMeters), nil
+	p, err := s.Pressure(oss)
+	if err != nil {
+		return 0, err
+	}
+	return calcPressurePascalSealevel(p, altitudeMeters), nil
 }
 
 func readRawTemp(s *Sensor) (uint16, error) {
-    err := s.dev.WriteReg(regControl, []byte{cmdReadTemp})
+	err := s.dev.WriteReg(regControl, []byte{cmdReadTemp})
 
-    time.Sleep(4500 * time.Microsecond)
+	time.Sleep(4500 * time.Microsecond)
 
-    buf := make([]byte, 2, 2)
-    err = s.dev.ReadReg(regTempOrPressure, buf)
-    if err != nil {
-        return 0, err
-    }
-    return uint16(buf[0])<<8 + uint16(buf[1]), nil
+	buf := make([]byte, 2, 2)
+	err = s.dev.ReadReg(regTempOrPressure, buf)
+	if err != nil {
+		return 0, err
+	}
+	return uint16(buf[0])<<8 + uint16(buf[1]), nil
 }
 
 func readRawPressure(s *Sensor, oss uint8) (msb byte, lsb byte, xlsb byte, err error) {
 
-    var cmd byte
-    cmd = cmdReadPressure + (oss << 6)
+	var cmd byte
+	cmd = cmdReadPressure + (oss << 6)
 
-    t1 := time.Now()
-    err = s.dev.WriteReg(regControl, []byte{cmd}) // start the measurement
-    if err != nil {
-        return
-    }
-    accessDuration := time.Since(t1)
+	t1 := time.Now()
+	err = s.dev.WriteReg(regControl, []byte{cmd}) // start the measurement
+	if err != nil {
+		return
+	}
+	accessDuration := time.Since(t1)
 
-    // Testing a real sensor with I2C bus clock of 100kHz on a Raspberry PI 2 shows that
-    // ReadReg blocks about 9ms (Linux bus access) + 2ms per transmitted word.
-    // Thus, we minimize calls to ReadReg and read as many bytes as possible at once.
+	// Testing a real sensor with I2C bus clock of 100kHz on a Raspberry PI 2 shows that
+	// ReadReg blocks about 9ms (Linux bus access) + 2ms per transmitted word.
+	// Thus, we minimize calls to ReadReg and read as many bytes as possible at once.
 
-    // fmt.Printf("WRITE TIME %v\n", accessDuration)
+	// fmt.Printf("WRITE TIME %v\n", accessDuration)
 
-    buf := make([]byte, 5) // storage for registers 0xF4..0xF8
+	buf := make([]byte, 5) // storage for registers 0xF4..0xF8
 
-    var delay time.Duration
-    // typical measurement times from Bosch BMP180 datasheet
-    switch oss {
-    case 0:
-        delay = 4500
-    case 1:
-        delay = 7500
-    case 2:
-        delay = 13500
-    case 3:
-        delay = 25500
-    }
+	var delay time.Duration
+	// typical measurement times from Bosch BMP180 datasheet
+	switch oss {
+	case 0:
+		delay = 4500
+	case 1:
+		delay = 7500
+	case 2:
+		delay = 13500
+	case 3:
+		delay = 25500
+	}
 
-    // We sleep for the suggested measurement time minus the bus access time which acts as an 'implicit sleep'
-    time.Sleep(delay*time.Microsecond - accessDuration)
+	// We sleep for the suggested measurement time minus the bus access time which acts as an 'implicit sleep'
+	time.Sleep(delay*time.Microsecond - accessDuration)
 
-    //t2 := time.Now() // debug
+	//t2 := time.Now() // debug
 
-    err = s.dev.ReadReg(regControl, buf) // read registers 0xF4..0xF8 (9ms + 5 * 2ms = 19ms)
-    if err != nil {
-        return
-    }
+	err = s.dev.ReadReg(regControl, buf) // read registers 0xF4..0xF8 (9ms + 5 * 2ms = 19ms)
+	if err != nil {
+		return
+	}
 
-    // The chip will change the control register 0xF4 when measurement is done. We poll for this.
-    // Because we slept for the suggested time, this loop should not run, but it is here
-    // in case there is an unexpected delay in the conversion.
-    for buf[0]&0x20 > 0 {
-        // wait for SCO bit to be cleared
-        //fmt.Printf("LOOP %v %b\n", time.Since(t1), buf[0])
-        err = s.dev.ReadReg(regControl, buf) // read registers 0xF4..0xF8 (9ms + 5 * 2ms = 19ms)
-        if err != nil {
-            return
-        }
-    }
+	// The chip will change the control register 0xF4 when measurement is done. We poll for this.
+	// Because we slept for the suggested time, this loop should not run, but it is here
+	// in case there is an unexpected delay in the conversion.
+	for buf[0]&0x20 > 0 {
+		// wait for SCO bit to be cleared
+		//fmt.Printf("LOOP %v %b\n", time.Since(t1), buf[0])
+		err = s.dev.ReadReg(regControl, buf) // read registers 0xF4..0xF8 (9ms + 5 * 2ms = 19ms)
+		if err != nil {
+			return
+		}
+	}
 
-    // At this point, measurement is guaranteed to be completed
-    //fmt.Printf("DONE %v\n", time.Since(t2)) // debug
+	// At this point, measurement is guaranteed to be completed
+	//fmt.Printf("DONE %v\n", time.Since(t2)) // debug
 
-    msb = buf[2]  // register 0xF6
-    lsb = buf[3]  // register 0xF7
-    xlsb = buf[4] // register 0xF8
-    return
+	msb = buf[2]  // register 0xF6
+	lsb = buf[3]  // register 0xF7
+	xlsb = buf[4] // register 0xF8
+	return
 }
 
 func readCalibration(s *Sensor) (err error) {
-    buf := make([]byte, calibrationNumWords, calibrationNumWords)
-    err = s.dev.ReadReg(regCalibration, buf)
-    if err != nil {
-        return
-    }
+	buf := make([]byte, calibrationNumWords, calibrationNumWords)
+	err = s.dev.ReadReg(regCalibration, buf)
+	if err != nil {
+		return
+	}
 
-    s.calib.ac1 = readInt16(buf[0:2])
-    s.calib.ac2 = readInt16(buf[2:4])
-    s.calib.ac3 = readInt16(buf[4:6])
+	s.calib.ac1 = readInt16(buf[0:2])
+	s.calib.ac2 = readInt16(buf[2:4])
+	s.calib.ac3 = readInt16(buf[4:6])
 
-    s.calib.ac4 = readUint16(buf[6:8])
-    s.calib.ac5 = readUint16(buf[8:10])
-    s.calib.ac6 = readUint16(buf[10:12])
+	s.calib.ac4 = readUint16(buf[6:8])
+	s.calib.ac5 = readUint16(buf[8:10])
+	s.calib.ac6 = readUint16(buf[10:12])
 
-    s.calib.b1 = readInt16(buf[12:14])
-    s.calib.b2 = readInt16(buf[14:16])
-    s.calib.mb = readInt16(buf[16:18])
-    s.calib.mc = readInt16(buf[18:20])
-    s.calib.md = readInt16(buf[20:22])
+	s.calib.b1 = readInt16(buf[12:14])
+	s.calib.b2 = readInt16(buf[14:16])
+	s.calib.mb = readInt16(buf[16:18])
+	s.calib.mc = readInt16(buf[18:20])
+	s.calib.md = readInt16(buf[20:22])
 
-    return nil
+	return nil
 }
 
 func readInt16(data []byte) (ret int16) {
-    buf := bytes.NewBuffer(data)
-    binary.Read(buf, binary.BigEndian, &ret)
-    return
+	buf := bytes.NewBuffer(data)
+	binary.Read(buf, binary.BigEndian, &ret)
+	return
 }
 
 func readUint16(data []byte) (ret uint16) {
-    buf := bytes.NewBuffer(data)
-    binary.Read(buf, binary.BigEndian, &ret)
-    return
+	buf := bytes.NewBuffer(data)
+	binary.Read(buf, binary.BigEndian, &ret)
+	return
 }
--- calc.go
+++ calc.go
@@ -8,50 +8,50 @@
 package bmp180
 
 import (
-    "math"
+	"math"
 )
 
 func calcTempCelsius(ut uint16, calib calibration) float64 {
-    x1 := int(ut - calib.ac6) * int(calib.ac5) / (1 << 15)
-    x2 := int(calib.mc) * (1 << 11) / (x1 + int(calib.md))
-    b5 := x1 + x2
-    t := float64((b5 + 8) / (1 << 4)) / 10.0
-    return t
+	x1 := int(ut - calib.ac6) * int(calib.ac5) / (1 << 15)
+	x2 := int(calib.mc) * (1 << 11) / (x1 + int(calib.md))
+	b5 := x1 + x2
+	t := float64((b5 + 8) / (1 << 4)) / 10.0
+	return t
 }
 
 func calcPressurePascal(ut uint16, tempCelsius float64, msb, lsb, xlsb byte, oss uint8, calib calibration) float64 {
-    msbInt := int64(msb)
-    lsbInt := int64(lsb)
-    xlsbInt := int64(xlsb)
-
-    up := ((msbInt) << 16 + (lsbInt) << 8 + (xlsbInt)) >> (8 - oss)
-    x1 := (int64(ut) - int64(calib.ac6)) * int64(calib.ac5) / (1 << 15)
-    x2 := int64(calib.mc) * (1 << 11) / (x1 + int64(calib.md))
-    b5 := x1 + x2
-
-    b6 := b5 - 4000
-    x1 = int64(calib.b2) * (b6 * b6 / (1 << 12)) / (2 << 11)
-    x2 = int64(calib.ac2) * b6 / (1 << 11)
-    x3 := x1 + x2
-    b3 := (((int64(calib.ac1) * 4 + x3) << oss) + 2) / 4
-    x1 = int64(calib.ac3) * b6 / (1 << 13)
-    x2 = (int64(calib.b1) * (b6 * b6 / (1 << 12))) / (1 << 16)
-    x3 = ((x1 + x2) + 2) / 4
-    b4 := uint64(calib.ac4) * (uint64(x3 + 32768)) / (1 << 15)
-    b7 := (uint64(up) - uint64(b3)) * (50000 >> oss)
-    p := int64(0)
-    if b7 < (1 << 31) {
-        p= int64(b7 * 2 / b4)
-    } else {
-        p= int64(b7 / b4 * 2)
-    }
-    x1 = (p >> 8) * (p >> 8)
-    x1 = x1 * 3038 / (1 << 16)
-    x2 = (-7357 * p) / (1 << 16)
-    rslt := float64(p + (x1 + x2 + 3791) / (1 << 4)) / 100.0
-    return rslt
+	msbInt := int64(msb)
+	lsbInt := int64(lsb)
+	xlsbInt := int64(xlsb)
+	
+	up := ((msbInt) << 16 + (lsbInt) << 8 + (xlsbInt)) >> (8 - oss)
+	x1 := (int64(ut) - int64(calib.ac6)) * int64(calib.ac5) / (1 << 15)
+	x2 := int64(calib.mc) * (1 << 11) / (x1 + int64(calib.md))
+	b5 := x1 + x2
+	
+	b6 := b5 - 4000
+	x1 = int64(calib.b2) * (b6 * b6 / (1 << 12)) / (2 << 11)
+	x2 = int64(calib.ac2) * b6 / (1 << 11)
+	x3 := x1 + x2
+	b3 := (((int64(calib.ac1) * 4 + x3) << oss) + 2) / 4
+	x1 = int64(calib.ac3) * b6 / (1 << 13)
+	x2 = (int64(calib.b1) * (b6 * b6 / (1 << 12))) / (1 << 16)
+	x3 = ((x1 + x2) + 2) / 4
+	b4 := uint64(calib.ac4) * (uint64(x3 + 32768)) / (1 << 15)
+	b7 := (uint64(up) - uint64(b3)) * (50000 >> oss)
+	p := int64(0)
+	if b7 < (1 << 31) {
+		p= int64(b7 * 2 / b4)
+	} else {
+		p= int64(b7 / b4 * 2)
+	}
+	x1 = (p >> 8) * (p >> 8)
+	x1 = x1 * 3038 / (1 << 16)
+	x2 = (-7357 * p) / (1 << 16)
+	rslt := float64(p + (x1 + x2 + 3791) / (1 << 4)) / 100.0
+	return rslt
 }
 
 func calcPressurePascalSealevel(p float64, altitudeMeters float64) float64 {
-    return p / math.Pow(1.0-altitudeMeters/44330.0, 5.255)
+	return p / math.Pow(1.0-altitudeMeters/44330.0, 5.255)
 }
